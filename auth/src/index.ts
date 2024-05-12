217e523ced95bf6e3667e48aa5c15c15fe587204
HITAYEZU Jean Pierre <hitlapidis2@gmail.com>
2024-05-13 00:03:23 +0200

    new

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -15,7 +15,7 @@ const swaggerui=require("swagger-ui-express");
 
 const app = express()
 dotenv.config();
-const PORT = 5000 
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({
     credentials: true,
